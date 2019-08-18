37dfb4765f262ec48c1c2787db254b969ffda615
정영준 <jyj0629@gmail.com>
2019-08-19 07:39:26 +0900

    First Study

--- app/src/main/java/com/example/recyclerviewstudy/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewstudy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.recyclerviewstudy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Bundle;
+//TEST
 
 public class MainActivity extends AppCompatActivity {
 
