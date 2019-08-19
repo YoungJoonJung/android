64366ad35f3fb0627569c4007fb4b80a321f57c0
정영준 <jyj0629@gmail.com>
2019-08-19 11:10:08 +0900

    Now Make

---
+++ app/src/main/java/com/example/recyclerviewstudy/GreenAdapter.java
@@ -0,0 +1,11 @@
+package com.example.recyclerviewstudy;
+
+
+import android.support.v7.widget.RecyclerView;
+
+public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
+
+    public class NumberViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
