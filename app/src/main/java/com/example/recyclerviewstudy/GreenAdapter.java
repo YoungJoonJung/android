c37a28d89255fa38224a3ee74ca742dbbe48b45e
정영준 <jyj0629@gmail.com>
2019-08-19 12:02:35 +0900

    Now Make

--- app/src/main/java/com/example/recyclerviewstudy/GreenAdapter.java
+++ app/src/main/java/com/example/recyclerviewstudy/GreenAdapter.java
@@ -1,11 +1,56 @@
 package com.example.recyclerviewstudy;
 
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
+    private  int mNumberItems;
 
-    public class NumberViewHolder extends RecyclerView.ViewHolder {
+    public GreenAdapter(int numberOfItem) {
+        mNumberItems = numberOfItem;
+    }
 
+    @NonNull
+    @Override
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.number_list_view;
+        LayoutInflater inFlater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = View.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        NumberViewHolder viewHolder = new NumberViewHolder(view);
+
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NumberViewHolder numberViewHolder, int position) {
+        numberViewHolder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    class NumberViewHolder extends RecyclerView.ViewHolder{
+        TextView listItemNumberView;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+
+            listItemNumberView = (TextView) itemView.findViewById(R.id.number_list_view);
+        }
+        void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
     }
 }
