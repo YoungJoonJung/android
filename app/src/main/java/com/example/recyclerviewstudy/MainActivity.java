008806170e3ada31f274cfeae5f19281d8fcbdee
정영준 <jyj0629@gmail.com>
2019-08-19 22:32:48 +0900

    Now Make

--- app/src/main/java/com/example/recyclerviewstudy/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewstudy/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements GreenAdapter.List
 
     private GreenAdapter mAdapter;
     private RecyclerView mNumberList;
+    private Menu mMenu;
 
     private Toast mToast;
 
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements GreenAdapter.List
         setContentView(R.layout.activity_main);
 
         mNumberList = (RecyclerView) findViewById(R.id.tv_recyclerview);
+        mMenu = (Menu) findViewById(R.id.refreh_button);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mNumberList.setLayoutManager(layoutManager);
@@ -33,6 +37,26 @@ public class MainActivity extends AppCompatActivity implements GreenAdapter.List
         mAdapter = new GreenAdapter(NUM_LIST_ITEMS, this);
 
         mNumberList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.refresh_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        if(itemId == R.id.refreh_button) {
+            mAdapter = new GreenAdapter(NUM_LIST_ITEMS, this);
+            mNumberList.setAdapter(mAdapter);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
